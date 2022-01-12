f7ffca01245deb599f82ddb301737a27da2e28b9
Alex Krueger <alexckrueger@gmail.com>
2022-01-12 16:55:30 -0600

    adds code to actually use ffaker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 # Actor.create({first_name: "Big", last_name: "Bad", known_for: "Wolf"})
 # Actor.create({first_name: "Mary", last_name: "Had a", known_for: "Little Lamb"})
+
+require 'ffaker'
+
+10.times do 
+  Actor.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+end
