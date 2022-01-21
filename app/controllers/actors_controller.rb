a051566b8857ff1dc9f45c0fd972f7872cf8ff84
Alex Krueger <alexckrueger@gmail.com>
2022-01-20 18:39:04 -0600

    adds validation and happy/sad paths

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -13,8 +13,11 @@ class ActorsController < ApplicationController
       gender: params[:gender],
       age: params[:age]
     )
-    actor.save
-    render json: actor
+    if actor.save
+      render json: actor
+    else
+      render json: {errors: actor.errors.full_messages}
+    end
   end
 
   def show
@@ -24,14 +27,16 @@ class ActorsController < ApplicationController
 
   def update
     actor = Actor.find(params[:id])
-    actor.update(
-      first_name: params[:first_name] || actor.first_name,
-      last_name: params[:last_name] || actor.last_name,
-      known_for: params[:known_for] || actor.known_for,
-      gender: params[:gender] || actor.gender,
-      age: params[:age] || actor.age
-    )
-    render json: actor
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
+    actor.known_for = params[:known_for] || actor.known_for
+    actor.gender = params[:gender] || actor.gender
+    actor.age = params[:age] || actor.age
+    if actor.save
+      render json: actor
+    else
+      render json: {errors: actor.errors.full_messages}
+    end
   end
 
   def destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, length: {minimum: 2}, uniqueness: true
+  validates :year, numericality: {greater_than: 1800}
+  validates :plot, presence: true
+  validates :director, presence: true
 end
